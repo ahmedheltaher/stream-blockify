f0be5678b7d95271be520566c2eb6e92edf40218
Ahmed Eltaher <ahmedheltaher@gmail.com>
2025-03-15 03:09:47 +0200

    CHORE: reduced coverageThresholds

--- jest.config.ts
+++ jest.config.ts
@@ -42,10 +42,10 @@ const config: Config = {
 	// Coverage thresholds
 	coverageThreshold: {
 		global: {
-			branches: 70,
-			functions: 70,
-			lines: 70,
-			statements: 70
+			branches: 65,
+			functions: 65,
+			lines: 65,
+			statements: 65
 		}
 	},
 
